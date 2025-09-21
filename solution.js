0a36fb58be1d6b54b55d545391b4f68b2cace51e
Vladimir Gulev <vladinson009@icloud.com>
2025-09-21 22:25:29 +0200

    Format files with Pretifiier

--- solution.js
+++ solution.js
@@ -1,23 +1,26 @@
-let reservation =
-{
-    startDate: null,
-    endDate: null,
-    guestsCount: 0,
-    roomType: null,
-    name: null,
-    phone: null,
-    email: null
-}
+let reservation = {
+  startDate: null,
+  endDate: null,
+  guestsCount: 0,
+  roomType: null,
+  name: null,
+  phone: null,
+  email: null,
+};
 
 function changeContent(className) {
-    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
+  document
+    .querySelectorAll('.custom-form')
+    .forEach((div) => div.classList.add('hidden'));
+  if (document.querySelector(`.${className}`) != null) {
     document.querySelector(`.${className}`).classList.remove('hidden');
-    }
+  }
 }
 
-document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
+document
+  .querySelector('#new-reservation')
+  .addEventListener('click', (e) => cleanData(e));
 
 function cleanData(e) {
-    changeContent('search-form-content');
+  changeContent('search-form-content');
 }
