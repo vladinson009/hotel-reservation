1ccda7de9642b2a06ded23c26dadf245ab2c7f89
Vladimir Gulev <vladinson009@icloud.com>
2025-09-21 22:24:20 +0200

    Veriifier job is done here

--- solution.js
+++ solution.js
@@ -1,23 +1,46 @@
-let reservation =
-{
-    startDate: null,
-    endDate: null,
-    guestsCount: 0,
-    roomType: null,
-    name: null,
-    phone: null,
-    email: null
-}
+let reservation = {
+  startDate: null,
+  endDate: null,
+  guestsCount: 0,
+  roomType: null,
+  name: null,
+  phone: null,
+  email: null,
+};
 
 function changeContent(className) {
-    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
+  document
+    .querySelectorAll('.custom-form')
+    .forEach((div) => div.classList.add('hidden'));
+  if (document.querySelector(`.${className}`) != null) {
     document.querySelector(`.${className}`).classList.remove('hidden');
-    }
+  }
 }
 
-document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
+document
+  .querySelector('#new-reservation')
+  .addEventListener('click', (e) => cleanData(e));
 
 function cleanData(e) {
-    changeContent('search-form-content');
+  changeContent('search-form-content');
 }
+
+//From Verifier => Start
+document
+  .querySelector('#confirm-back-btn')
+  .addEventListener('click', (e) => getBackToPersonalData(e));
+
+function getBackToPersonalData(e) {
+  e.preventDefault();
+  changeContent('guest-details-form-content');
+}
+
+document
+  .querySelector('#confirm-reservation')
+  .addEventListener('click', (e) => showThanksPage(e));
+
+function showThanksPage(e) {
+  e.preventDefault();
+  changeContent('thank-you-content');
+}
+// From Verifier => End
